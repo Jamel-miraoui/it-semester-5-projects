9d717d0aebbfb4585cc7d3772afbcef0445810a4
Miraoui Jamel <105128037+Jamel-miraoui@users.noreply.github.com>
2023-10-26 01:36:28 +0100

    b

--- Integration project/tp2/index.php
+++ Integration project/tp2/index.php
@@ -19,33 +19,34 @@ if ($statut == "succes") {
 <body class="index-page">
   <form method="post" action="index.php">
   <label for="filterNumRatV">NumRatV:</label>
-  <input type="text" name="filterNumRatV" id="filterNumRatV">
+  <input type="text" name="filterNumRatV" id="filterNumRatV" value="<?php echo $NumRatV ?>">
   
   <label for="filterMatProf">MatProf:</label>
-  <input type="text" name="filterMatProf" id="filterMatProf">
+  <input type="text" name="filterMatProf" id="filterMatProf" value="<?php echo $MatProf ?>">
 
   <label for="filterDateRat">DateRat:</label>
-  <input type="date" name="filterDateRat" id="filterDateRat">
+  <input type="date" name="filterDateRat" id="filterDateRat"value="<?php echo $DateRat ?>">
   
   <label for="filterSeance">Seance:</label>
-  <input type="text" name="filterSeance" id="filterSeance">
+  <input type="text" name="filterSeance" id="filterSeance" value="<?php echo $Seance ?>">
 
   <label for="filterSession">Session:</label>
-  <input type="text" name="filterSession" id="filterSession"> 
+  <input type="text" name="filterSession" id="filterSession"value="<?php echo $Session ?>"> 
+
   <label for="filterSalle">Salle:</label>
-  <input type="text" name="filterSalle" id="filterSalle">
+  <input type="text" name="filterSalle" id="filterSalle"value="<?php echo $Salle ?>">
 
   <label for="filterJour">Jour:</label>
-  <input type="text" name="filterJour" id="filterJour">
+  <input type="text" name="filterJour" id="filterJour"value="<?php echo $Jour ?>">
 
   <label for="filterCodeClasse">CodeClasse:</label>
-  <input type="text" name="filterCodeClasse" id="filterCodeClasse">
+  <input type="text" name="filterCodeClasse" id="filterCodeClasse" value="<?php echo $CodeClasse ?>">
 
   <label for="filterCodeMatiere">CodeMatiere:</label>
-  <input type="text" name="filterCodeMatiere" id="filterCodeMatiere">
+  <input type="text" name="filterCodeMatiere" id="filterCodeMatiere" value="<?php echo $CodeMatiere ?>">
 
   <label for="filterEtat">Etat:</label>
-  <input type="text" name="filterEtat" id="filterEtat">
+  <input type="text" name="filterEtat" id="filterEtat" value="<?php echo $Etat ?>">
 
   <input type="submit" value="Apply Change">
 </form>
--- Integration project/tp2/indexF.php
+++ Integration project/tp2/indexF.php
@@ -12,18 +12,19 @@ if ($statut == "succes") {
   $Etat = $_POST["filterEtat"];
   $header = "addForm";
 
+  $sqldate = "LIKE $DateRat ";
 
   function printResult()
   {
     $filterR = 0;
     $filter = [];
     global $mysqli;
-    global $result, $MatProf, $DateRat, $Seance, $Session, $Salle, $Jour, $CodeClasse, $CodeMatiere,$Etat, $NumRatV;
-    $table = [$NumRatV, $MatProf, $DateRat, $Seance, $Session, $Salle, $Jour, $CodeClasse, $CodeMatiere,$Etat];
-
+    global $result, $MatProf, $DateRat, $Seance, $Session, $Salle, $Jour, $CodeClasse, $CodeMatiere, $Etat, $NumRatV;
+    $table = [$NumRatV, $MatProf, $DateRat, $Seance, $Session, $Salle, $Jour, $CodeClasse, $CodeMatiere, $Etat];
+    $tableNames = ["NumRatV", "MatProf", "DATE(DateRat)", "Seance", "Session", "Salle", "Jour", "CodeClasse", "CodeMatiere", "Etat"];
     for ($i = 0; $i < count($table); $i++) {
       if ($table[$i] !== "" && !is_null($table[$i])) {
-        $filter[] = $table[$i];
+        $filter[] = "$tableNames[$i]=$table[$i]";
         $filterR = 1;
       }
     }
@@ -31,28 +32,54 @@ if ($statut == "succes") {
     foreach ($filter as $value) {
       echo $value . "<br>";
     }
-    if ($filterR == 0) {
-      $req = "SELECT * from RatVol";
-      $result = $mysqli->query($req);
-      while ($row = $result->fetch_assoc()) {
-        echo "<tr>";
-        echo "<td>" . $row['NumRatV'] . "</td>";
-        echo "<td>" . $row['MatProf'] . "</td>";
-        echo "<td>" . $row['DateRat'] . "</td>";
-        echo "<td>" . $row['Seance'] . "</td>";
-        echo "<td>" . $row['Session'] . "</td>";
-        echo "<td>" . $row['Salle'] . "</td>";
-        echo "<td>" . $row['Jour'] . "</td>";
-        echo "<td>" . $row['CodeClasse'] . "</td>";
-        echo "<td>" . $row['CodeMatiere'] . "</td>";
-        echo "<td>" . $row['Etat'] . "</td>";
-        echo '<td><a style="color: blue;" href="editForm.php?id=' . $row['NumRatV'] . '">Edit</a> | <a style="color: red;"  href="delete.php?id=' . $row['NumRatV'] . '">Delete</a></td>';
-        echo "</tr>";
+
+    $filterCount = count($filter);
+
+    if ($filterR == 1 && $filterCount == 1) {
+      foreach ($filter as $champ) {
+        $reqf =  "SELECT * FROM RatVol WHERE $champ ;";
+      }
+    }
+    if ($filterR == 1 && $filterCount > 1) {
+      $i = 0;
+      $champs = "";
+      foreach ($filter as $champ) {
+        $i++;
+        $champs .=  "$champ ";
+        if ($i  == $filterCount) {
+          break;
+        }
+        $champs .=  "AND ";
       }
     }
 
+    $reqf = "SELECT * FROM RatVol WHERE $champs ;";
+    echo $reqf;
+
+    if ($filterR == 0) {
+      $req = "SELECT * from RatVol";
+    } else if ($filterR == 1 && $filterCount == 1) {
+      $req =  "SELECT * FROM RatVol WHERE $champ ;";
+    } else {
+      $req = $reqf;
+    }
+    $result = $mysqli->query($req);
+    while ($row = $result->fetch_assoc()) {
+      echo "<tr>";
+      echo "<td>" . $row['NumRatV'] . "</td>";
+      echo "<td>" . $row['MatProf'] . "</td>";
+      echo "<td>" . $row['DateRat'] . "</td>";
+      echo "<td>" . $row['Seance'] . "</td>";
+      echo "<td>" . $row['Session'] . "</td>";
+      echo "<td>" . $row['Salle'] . "</td>";
+      echo "<td>" . $row['Jour'] . "</td>";
+      echo "<td>" . $row['CodeClasse'] . "</td>";
+      echo "<td>" . $row['CodeMatiere'] . "</td>";
+      echo "<td>" . $row['Etat'] . "</td>";
+      echo '<td><a style="color: blue;" href="editForm.php?id=' . $row['NumRatV'] . '">Edit</a> | <a style="color: red;"  href="delete.php?id=' . $row['NumRatV'] . '">Delete</a></td>';
+      echo "</tr>";
+    }
   }
 } else {
   echo $statut;
 }
-

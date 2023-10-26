0d8ea45c2b280894c498a216f5ac63d2a206e706
Miraoui Jamel <105128037+Jamel-miraoui@users.noreply.github.com>
2023-10-26 20:24:35 +0100

    last tauch

--- Integration project/tp2/indexF.php
+++ Integration project/tp2/indexF.php
@@ -44,27 +44,37 @@ if ($statut == "succes") {
         }
         $champs .=  "AND ";
       }
+
       $req = "SELECT * FROM RatVol WHERE $champs ;";
-      echo $req ;
     }
-    $result = $mysqli->query($req);
-    while ($row = $result->fetch_assoc()) {
-      echo "<tr>";
-      echo "<td>" . $row['NumRatV'] . "</td>";
-      echo "<td>" . $row['MatProf'] . "</td>";
-      echo "<td>" . $row['DateRat'] . "</td>";
-      echo "<td>" . $row['Seance'] . "</td>";
-      echo "<td>" . $row['Session'] . "</td>";
-      echo "<td>" . $row['Salle'] . "</td>";
-      echo "<td>" . $row['Jour'] . "</td>";
-      echo "<td>" . $row['CodeClasse'] . "</td>";
-      echo "<td>" . $row['CodeMatiere'] . "</td>";
-      echo "<td>" . $row['Etat'] . "</td>";
-      echo '<td><a style="color: blue;" href="editForm.php?id=' . $row['NumRatV'] . '">Edit</a> | <a style="color: red;"  href="delete.php?id=' . $row['NumRatV'] . '">Delete</a></td>';
-      echo "</tr>";
+    try {
+      $count = 0;
+      $result = $mysqli->query($req);
+      while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row['NumRatV'] . "</td>";
+        echo "<td>" . $row['MatProf'] . "</td>";
+        echo "<td>" . $row['DateRat'] . "</td>";
+        echo "<td>" . $row['Seance'] . "</td>";
+        echo "<td>" . $row['Session'] . "</td>";
+        echo "<td>" . $row['Salle'] . "</td>";
+        echo "<td>" . $row['Jour'] . "</td>";
+        echo "<td>" . $row['CodeClasse'] . "</td>";
+        echo "<td>" . $row['CodeMatiere'] . "</td>";
+        echo "<td>" . $row['Etat'] . "</td>";
+        echo '<td><a style="color: blue;" href="editForm.php?id=' . $row['NumRatV'] . '">Edit</a> | <a style="color: red;"  href="delete.php?id=' . $row['NumRatV'] . '">Delete</a></td>';
+        echo "</tr>";
+        $count++;
+      }
+      if ($count == 0) {
+        echo '<center><h4 style="color: red;">0 Result Found !!</h4></center>';
+      } else {
+        echo '<center><h4 style="color: green;">' . $count . ' Result Found !!</h4></center>';
+      }
+    } catch (Exception $e) {
+      echo '<center><h4 style="color: red;">0 Result Found !!</h4></center>';
     }
   }
-
 } else {
   echo $statut;
 }

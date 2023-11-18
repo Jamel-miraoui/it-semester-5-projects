8dbb1b75d0146d58143a9b38b4d9a627e1438514
Miraoui Jamel <105128037+Jamel-miraoui@users.noreply.github.com>
2023-11-18 22:37:09 +0100

    add tp4 powerbi

---
+++ ASP.net/tp2/WebApplication1/WebApplication1/Models/Groupe.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Groupe
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        [Display(Name = "Groupe")]
+        public string LibGroupe { get; set; }
+        public virtual ICollection<Etudiant> Etudiants { get; set; }
+    }
+}
---
+++ ASP.net/tp2/WebApplication1/WebApplication1/Models/Matiere.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Matiere
+    {
+        public int Id { get; set; }
+        [Display(Name = "Matière")]
+        [Required, StringLength(30, MinimumLength = 3)]
+        public string LibMatiere { get; set; }
+        public virtual ICollection<Inscription> Inscriptions { get; set; }
+    }
+}

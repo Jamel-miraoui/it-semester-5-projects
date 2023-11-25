f42cf34cd02949442b12eaed438b6b26aef251f0
Miraoui Jamel <105128037+Jamel-miraoui@users.noreply.github.com>
2023-11-25 23:21:38 +0100

    !!

---
+++ ASP.net/tp1/tp1/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tp1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name , int numTimes=1)
+        {
+            return "hello" + name + "your NumTimes is" + numTimes; 
+        }
+    }
+}
